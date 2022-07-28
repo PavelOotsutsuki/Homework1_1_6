46966b8fbd848dcf48e710f7b52a7fd5d70210a8
PavelOotsutsuki <109985069+PavelOotsutsuki@users.noreply.github.com>
2022-07-28 17:55:12 +0300

    Добавьте файлы проекта.

---
+++ Homework1_1_6/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Homework1_1_6
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int gold;
+            int crystal=0;
+            int transactionValue;
+            int price=12;
+
+            Console.Write("Введите, сколько у Вас золота: ");
+            gold=Convert.ToInt32(Console.ReadLine());
+            Console.Write("Сколько кристаллов Вы хотите купить по цене " + price + "? ");
+            transactionValue = Convert.ToInt32(Console.ReadLine());
+            crystal += transactionValue;
+            gold -= transactionValue * price;
+            Console.WriteLine("У Вас " + crystal + " кристаллов и " + gold + " золота");
+        }
+    }
+}
